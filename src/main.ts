9acb3017b9053fb54c20873a35a91d73893b8ae6
Aditya T <adityatelsinge89@gmail.com>
2025-11-07 23:19:01 +0530

    Allow all origin

--- src/main.ts
+++ src/main.ts
@@ -12,11 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.enableCors({
-    origin: [
-      'https://ecommerce-frontend-five-kappa.vercel.app',
-      'http://localhost:3000',
-      'http://localhost:3001',
-    ],
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
